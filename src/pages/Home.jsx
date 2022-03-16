3af3e9a828d1a29fe6ee6b53beb36a7cc730ff61
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-03-16 07:59:19 -0300

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import Greeting from '../components/Greeting/Greeting.jsx';
+
+export default function Home() {
+	return <Greeting />;
+}
---
+++ src/pages/Recall.jsx
@@ -0,0 +1,3 @@
+export default function Recall() {
+  return <h1>Recall page</h1>;
+}
